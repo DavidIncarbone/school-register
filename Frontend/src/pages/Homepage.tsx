e319cb4cedf19d9880b5c70438e8049f8b139650
Aj-Herrera-99 <ajherrera999@gmail.com>
2025-06-05 16:10:04 +0200

    Refactor Homepage component; enhance input handling and streamline student retrieval logic

--- Frontend/src/pages/Homepage.tsx
+++ Frontend/src/pages/Homepage.tsx
@@ -4,12 +4,13 @@ import type { Course } from "../config/types";
 import { useSearchParams } from "react-router";
 
 export default function Homepage() {
+    console.log("render homepage");
     // vars, let const ... ...
     const [courses, setCourses] = useState<Course[] | null>(null);
     const [searchParams, setSearchParams] = useSearchParams();
+    const [inputData, setInputData] = useState("");
 
-    // console.log(searchParams.get("course"));
-
+    // actions
     // ? l'utente studente ricevera solo la propria scheda da questa chiamata
     const testStudentIndex = async () => {
         try {
@@ -20,68 +21,71 @@ export default function Homepage() {
         }
     };
 
-    const handleCourseSelected = async (e: ChangeEvent<HTMLSelectElement>) => {
-        const selectedCourseId = e.target.value;
+    const handleInputChange = async (e: ChangeEvent<HTMLInputElement>) => {
+        const last_name = e.target.value;
+        const key = "last_name";
+
         try {
             const res = await api.get(
-                `/api/students?course_id=${selectedCourseId}`
+                `/api/students?${key}=${selectedCourseId}`
             );
+            setSearchParams({
+                [key]: selectedCourseId,
+            });
             console.log(res.data);
         } catch (err) {
             console.error(err);
         }
     };
 
+    const handleCourseSelected = async (e: ChangeEvent<HTMLSelectElement>) => {
+        const selectedCourseId = e.target.value;
+        const key = "course_id";
+        try {
+            const res = await api.get(
+                `/api/students?${key}=${selectedCourseId}`
+            );
+            setSearchParams({
+                [key]: selectedCourseId,
+            });
+            console.log(res.data);
+        } catch (err) {
+            console.error(err);
+        }
+    };
+
+    // effects
     useEffect(() => {
         const fetchCourses = async () => {
             try {
                 const res = await api.get("/api/courses");
-                console.log(res.data);
                 setCourses(res.data.data as Course[]);
             } catch (err) {
                 console.error(err);
             }
         };
-
         fetchCourses();
     }, []);
 
     useEffect(() => {
-        // let queryString: { last_name?: string; course_id?: string } = {};
-        const last_name = searchParams.get("last_name");
-        const course_id = searchParams.get("course_id");
-        // if (searchParams.get("last_name")) {
-        //     queryString = {
-        //         ...queryString,
-        //         last_name: searchParams.get("last_name") ?? undefined,
-        //     };
-        // }
-        // if (searchParams.get("course_id")) {
-        //     queryString = {
-        //         ...queryString,
-        //         course_id: searchParams.get("course_id") ?? undefined,
-        //     };
-        // }
-
+        let queryStr = searchParams.toString();
+        queryStr = queryStr && "?" + queryStr;
+        // let params: Record<string, string> = {};
+        // searchParams.forEach((val, key) => {
+        //     params = { ...params, [key]: val };
+        // });
         const fetchStudents = async () => {
             try {
-                const res = await api.get(
-                    `/api/students?${last_name && "last_name=" + last_name}&${
-                        course_id && "course_id=" + course_id
-                    }}`
-                );
+                const res = await api.get(`/api/students${queryStr}`);
                 console.log(res.data);
             } catch (err) {
                 console.error(err);
             }
         };
-
-        fetchStudents();
-
-        // console.log(queryString);
-
-        // fetch /api/courses? ...queryString
-    }, []);
+        if (queryStr) {
+            fetchStudents();
+        }
+    }, [searchParams]);
 
     // view
     return (
@@ -93,6 +97,7 @@ export default function Homepage() {
             <div className="auth-form">
                 <div>
                     <input
+                        onChange={handleInputChange}
                         type="text"
                         name="last_name"
                         placeholder="Cerca per cognome"
