a1c7af5179224c0b64381b4681375b88ff3ee6fe
Aj-Herrera-99 <ajherrera999@gmail.com>
2025-06-15 23:12:11 +0200

    feat: update dependencies and enhance Header component; integrate lucide-react icons and improve logout handling

--- Frontend/src/partials/Header.tsx
+++ Frontend/src/partials/Header.tsx
@@ -1,10 +1,12 @@
-import { FaAddressCard, FaArrowCircleDown } from "react-icons/fa";
 import { api } from "../services/api";
 import { useGlobalStore } from "../store/useGlobalStore";
 import { Link, useNavigate } from "react-router";
+import { Bell, Calendar, LogOut, Megaphone } from "lucide-react";
+import { useQueryClient } from "@tanstack/react-query";
 
 export default function Header() {
     const navigate = useNavigate();
+    const queryClient = useQueryClient();
     const { authUser, setAuthUser, sidebarHidden, profile } = useGlobalStore();
 
     const handleLogout = async () => {
@@ -14,29 +16,37 @@ export default function Header() {
             await api.post("/logout");
         } catch (err) {
             console.error(err);
+        } finally {
+            queryClient.clear();
         }
     };
 
     return (
-        <header className="h-16 sticky top-0 bg-[#1e2125] flex flex-wrap z-40">
-            <div
-                className={`${
-                    sidebarHidden && "lg:translate-x-36"
-                } transition-transform duration-500 max-sm:hidden flex items-center grow px-4`}
-            >
-                <input
-                    type="text"
-                    className="w-full xl:w-1/2"
-                    placeholder="Search for something.."
-                />
-            </div>
-
+        <header
+            className={`${
+                !authUser && "justify-end"
+            } h-16 sticky top-0 bg-[#1e2125] flex flex-wrap z-40`}
+        >
             {authUser && (
-                <div className="flex items-center gap-6 px-4">
-                    <FaAddressCard className="size-8" />
-                    <FaAddressCard className="size-8" />
-                    <FaAddressCard className="size-8" />
-                </div>
+                <>
+                    <div
+                        className={`${
+                            sidebarHidden && "lg:translate-x-36"
+                        } transition-transform duration-500 max-sm:hidden flex items-center grow px-4`}
+                    >
+                        <input
+                            type="text"
+                            className="w-full xl:w-[500px]"
+                            placeholder="Search for something.."
+                        />
+                    </div>
+
+                    <div className="flex items-center gap-7 px-4">
+                        <Calendar className="size-6" />
+                        <Megaphone className="size-6" />
+                        <Bell className="size-6" />
+                    </div>
+                </>
             )}
 
             <div className="flex items-center gap-2 px-4">
@@ -46,7 +56,7 @@ export default function Header() {
                         className="w-12 aspect-square border rounded-full"
                     ></div>
                 )}
-                <div className="flex items-center gap-4">
+                <div className="flex items-center gap-7">
                     {profile && authUser ? (
                         <div className="flex flex-col">
                             <span className="capitalize">
@@ -67,7 +77,7 @@ export default function Header() {
                         <Link to="/login">Sign in</Link>
                     )}
                     {authUser && (
-                        <FaArrowCircleDown
+                        <LogOut
                             onClick={handleLogout}
                             className="cursor-pointer"
                         />
