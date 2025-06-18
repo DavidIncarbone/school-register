2c5dbf990c7625acd8f7604c016ec840e4399706
DavidIncarbone <david.incarbone@gmail.com>
2025-06-18 10:08:12 +0200

    add reset and close form to submit on assignment's page

--- Frontend/src/components/teacher/assignmentPage/AddAssignment.tsx
+++ Frontend/src/components/teacher/assignmentPage/AddAssignment.tsx
@@ -9,13 +9,13 @@ import { toast } from "react-hot-toast";
 import Loader from "@/components/ui/Loader";
 
 const schema = z.object({
-    body: z
-        .string()
-        .min(1, "Body field is required")
-        .max(255, "the maximum number of characters is 255"),
-    assignment_date: z.string().nonempty("Start's filed is required"),
-    deadline: z.string().nonempty("Deadline's filed is required"),
-    course_id: z.string().min(1),
+  body: z
+    .string()
+    .min(1, "Body field is required")
+    .max(255, "the maximum number of characters is 255"),
+  assignment_date: z.string().nonempty("Start's filed is required"),
+  deadline: z.string().nonempty("Deadline's filed is required"),
+  course_id: z.string().min(1),
 });
 // TYPES
 
@@ -23,162 +23,176 @@ const schema = z.object({
 type AssignmentFormData = z.infer<typeof schema>;
 
 type AddAssignmentProps = {
-    courses: Course[] | undefined;
-    queryParams: IndexAssignmentsParams;
-    isLoading: boolean;
-    // tipizzazione speciale per useState
-    setIsLoading: Dispatch<SetStateAction<boolean>>;
+  courses: Course[] | undefined;
+  queryParams: IndexAssignmentsParams;
+  isLoading: boolean;
+  // tipizzazione speciale per useState
+  setIsFormShowing: Dispatch<SetStateAction<boolean>>;
+  setIsLoading: Dispatch<SetStateAction<boolean>>;
 };
 
 export const AddAssignment = ({
-    courses,
-    queryParams,
+  courses,
+  queryParams,
+  setIsFormShowing,
 }: // isLoading,
 // setIsLoading,
 AddAssignmentProps) => {
-    console.log(courses);
+  console.log(courses);
 
-    // vars
-    const startDate = new Date().toISOString().split("T")[0];
-    const deadlineDate = new Date("2025/06/31").toISOString().split("T")[0];
-    const currentCourse = courses?.find(
-        (course) => course.id == queryParams.course_id
-    );
+  // vars
+  const startDate = new Date().toISOString().split("T")[0];
+  const deadlineDate = new Date("2025/06/31").toISOString().split("T")[0];
+  const currentCourse = courses?.find(
+    (course) => course.id == queryParams.course_id
+  );
+  const defaultValues = {
+    course_id: queryParams?.course_id,
+    assignment_date: "",
+    deadline: "",
+    body: "",
+  };
 
-    //   queries
-    const { mutate, isSuccess, isPending } =
-        useMutationStoreAssignment(queryParams);
+  //   queries
+  const {
+    mutate,
+    isSuccess,
+    isPending,
+    data: newAssignment,
+  } = useMutationStoreAssignment(queryParams);
 
-    useEffect(() => {
-        if (isSuccess) {
-            toast.success("ok");
-        }
-    }, [isSuccess]);
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+    reset,
+  } = useForm({
+    resolver: zodResolver(schema),
+    defaultValues,
+  });
 
-    // Validation
+  useEffect(() => {
+    if (isSuccess) {
+      toast.success("Assignment added successfully");
+      reset(defaultValues);
+      setIsFormShowing(false);
+    }
+  }, [isSuccess, reset]);
 
-    const {
-        register,
-        handleSubmit,
-        formState: { errors },
-    } = useForm({
-        resolver: zodResolver(schema),
-    });
+  // Validation
 
-    const createNewAssignment = async (formData: AssignmentFormData) => {
-        console.log(formData);
-        console.log(typeof queryParams?.course_id);
-        mutate(formData);
-        // toast.success("Assignment added succesfully");
-    };
-    return (
-        <section className="mt-5">
-            <h2 className="text-white text-2xl">
-                Add Assignment for{" "}
-                <span className="underline capitalize">
-                    {currentCourse?.name}
-                </span>
-            </h2>
-            <form
-                action=""
-                id="AssignmentForm"
-                className="w-full"
-                onSubmit={handleSubmit(createNewAssignment)}
-            >
-                <p className="text-gray-400 text-sm mb-3">
-                    The fields marked with * are required
-                </p>
-                <div className="grid grid-cols-3 gap-4 mb-3">
-                    <div className="flex flex-col gap-2 p-3">
-                        <div className="flex flex-col gap-0.5">
-                            <label htmlFor="start">Start*</label>
-                            <input
-                                type="date"
-                                // name="assignment_date"
-                                {...register("assignment_date")}
-                                id="start"
-                                max={startDate}
-                                className="border border-white p-3"
-                            />
-                        </div>
-                        {errors.body && (
-                            <p className="text-red-500 text-sm">
-                                {errors.assignment_date?.message}
-                            </p>
-                        )}
-                    </div>
-                    <div className="flex flex-col gap-2  p-3">
-                        <div className="flex flex-col gap-0.5 ">
-                            <label htmlFor="deadline">Deadline*</label>
-                            <input
-                                type="date"
-                                // name="deadline"
-                                {...register("deadline")}
-                                id="deadline"
-                                max={deadlineDate}
-                                className="border border-white p-3 "
-                            />
-                        </div>
-                        {errors.deadline && (
-                            <p className="text-red-500 text-sm">
-                                {errors.deadline.message}
-                            </p>
-                        )}
-                    </div>
+  const createNewAssignment = async (formData: AssignmentFormData) => {
+    console.log(formData);
+    console.log(typeof queryParams?.course_id);
+    mutate(formData);
+    // toast.success("Assignment added succesfully");
+  };
+  return (
+    <section className="mt-5">
+      <h2 className="text-white text-2xl">
+        Add Assignment for{" "}
+        <span className="underline capitalize">{currentCourse?.name}</span>
+      </h2>
+      <form
+        action=""
+        id="AssignmentForm"
+        className="w-full"
+        onSubmit={handleSubmit(createNewAssignment)}
+      >
+        <p className="text-gray-400 text-sm mb-3">
+          The fields marked with * are required
+        </p>
+        <div className="grid grid-cols-3 gap-4 mb-3">
+          <div className="flex flex-col gap-2 p-3">
+            <div className="flex flex-col gap-0.5">
+              <label htmlFor="start">Start*</label>
+              <input
+                type="date"
+                // name="assignment_date"
+                {...register("assignment_date")}
+                // value={startDate}
+                id="start"
+                max={startDate}
+                className="border border-white p-3"
+              />
+            </div>
+            {errors.body && (
+              <p className="text-red-500 text-sm">
+                {errors.assignment_date?.message}
+              </p>
+            )}
+          </div>
+          <div className="flex flex-col gap-2  p-3">
+            <div className="flex flex-col gap-0.5 ">
+              <label htmlFor="deadline">Deadline*</label>
+              <input
+                type="date"
+                // name="deadline"
+                {...register("deadline")}
+                // value={startDate}
+                id="deadline"
+                max={deadlineDate}
+                className="border border-white p-3 "
+              />
+            </div>
+            {errors.deadline && (
+              <p className="text-red-500 text-sm">{errors.deadline.message}</p>
+            )}
+          </div>
+        </div>
+        <div>
+          <label htmlFor="body">Body*</label>
+          <p className="text-xs text-white/80 pb-1">
+            Max. 10 rows | Max. 255 charachters
+          </p>
+          <textarea
+            // name="body"
+            id="body"
+            {...register("body")} // name assegnato tramite useForm
+            className="w-full border border-white p-3"
+            // value="prova"
+            rows={10}
+            maxLength={255}
+          ></textarea>
+          <div className="h-[20px]">
+            {errors.body && (
+              <p className="text-red-500 text-sm">{errors.body?.message}</p>
+            )}
+          </div>
+        </div>
+        <div className="flex gap-3 items-center mt-2">
+          <button
+            type="submit"
+            className={`btn-pretty ${isPending && "!cursor-not-allowed"}`}
+            disabled={isPending}
+          >
+            {isPending ? (
+              <>
+                <div className="absolute inset-0">
+                  <Loader isContained={true} />
                 </div>
-                <div>
-                    <label htmlFor="body">Body*</label>
-                    <p className="text-xs text-white/80 pb-1">
-                        Max. 10 rows | Max. 255 charachters
-                    </p>
-                    <textarea
-                        // name="body"
-                        id="body"
-                        {...register("body")} // name assegnato tramite useForm
-                        className="w-full border border-white p-3"
-                        rows={10}
-                        maxLength={255}
-                    ></textarea>
-                    <div className="h-[20px]">
-                        {errors.body && (
-                            <p className="text-red-500 text-sm">
-                                {errors.body?.message}
-                            </p>
-                        )}
-                    </div>
-                </div>
-                <div className="flex gap-3 items-center mt-2">
-                    <button
-                        type="submit"
-                        className={`btn-pretty ${
-                            isPending && "!cursor-not-allowed"
-                        }`}
-                        disabled={isPending}
-                    >
-                        {isPending ? (
-                            <>
-                                <div className="absolute inset-0">
-                                    <Loader isContained={true} />
-                                </div>
-                                <span className="text-transparent">
-                                    Add Assignment
-                                </span>
-                            </>
-                        ) : (
-                            "Add Assignment"
-                        )}
-                    </button>
-                    <button type="reset" className={`${isPending && "!cursor-not-allowed opacity-50"} btn-pretty`}>
-                        Reset
-                    </button>
-                </div>
-                {/* HIDDEN INPUT */}
-                <input
-                    type="hidden"
-                    {...register("course_id")}
-                    value={queryParams.course_id}
-                />
-            </form>
-        </section>
-    );
+                <span className="text-transparent">Add Assignment</span>
+              </>
+            ) : (
+              "Add Assignment"
+            )}
+          </button>
+          <button
+            type="reset"
+            className={`${
+              isPending && "!cursor-not-allowed opacity-50"
+            } btn-pretty`}
+          >
+            Reset
+          </button>
+        </div>
+        {/* HIDDEN INPUT */}
+        <input
+          type="hidden"
+          {...register("course_id")}
+          value={queryParams.course_id}
+        />
+      </form>
+    </section>
+  );
 };
